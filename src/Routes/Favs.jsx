2730feffb23da25783cee3ebba969e48178ef52d
markaboian <abomark09@gmail.com>
2023-04-10 16:19:58 -0300

    fixing issue with favorites view

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContextGlobal } from "../Components/utils/global.context";
 import Card from "../Components/Card";
 
 const Favs = () => {
 
-	const favorites = JSON.parse(localStorage.getItem("favorites"))
+	const { state } = useContext(ContextGlobal)
 
 	return (
 		<>
 			<h1>Dentists Favs</h1>
 			<div className="card-grid">
 				{
-					favorites.map(({name, username, id}) => {
+					state.data.map(({name, username, id}) => {
 						return (
 							<Card key={id} name={name} username={username}/>
 						)
